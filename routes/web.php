9690b994116c25f2e8c495b840d6366d1697ddd3
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-06 09:06:29 +0700

    Jobsheet 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,32 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return 'NIM: 2241760097 | Nama: Ahmad Iqbal FIrmansyah';
+});
+Route::get('/user/{iqbal}', function ($name){
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID){
+    return 'Pos ke-'.$postID." Komentar ke-".$commentID;
+});
+
+Route::get('/article/{id}', function ($id) 
+{
+    return 'Halaman Artikel dengan ID ' .$id;
+});
+Route::get('/user/{name?}', function ($name=null) { 
+return 'Nama saya '.$name; }); 
+
+Route::get('/user/{name?}', function ($name='John') 
+{ return 'Nama saya '.$name; }); 
+
