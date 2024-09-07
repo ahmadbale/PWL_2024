542310f7be293380830f23831c77af388da26e48
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-07 13:13:39 +0700

    Jobsheet 2 - Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,9 @@ Route::resource('photos', PhotoController::class);
 Route::resource('photos', PhotoController::class)->only([     'index', 'show' ]); 
 Route::resource('photos', PhotoController::class)->except([     'create', 'store', 'update', 'destroy' ]); 
  
+Route::get('/greeting', function () 
+{  
+    return view('blog.hello', ['name' => 'Iqbal']); });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']); 
+ 
